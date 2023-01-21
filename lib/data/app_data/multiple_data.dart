f1dc2fc68fa6b5654b7e36c2573f0327e16806fe
jankithummar <jankithummar2001@gmail.com>
2023-01-21 13:54:36 +0530

    second commite

---
+++ lib/data/app_data/multiple_data.dart
@@ -0,0 +1,86 @@
+class MutipleData {
+  static Map<String, dynamic> multipleData = {
+    "multipleList": [
+      {
+        "id": 1,
+        "name": "Rick Sanchez",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "origin": {
+          "name": "Earth (C-137)",
+          "url": "https://rickandmortyapi.com/api/location/1"
+        },
+        "location": {
+          "name": "Citadel of Ricks",
+          "url": "https://rickandmortyapi.com/api/location/3"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        "episode": [
+          "https://rickandmortyapi.com/api/episode/46",
+          "https://rickandmortyapi.com/api/episode/47",
+          "https://rickandmortyapi.com/api/episode/48",
+          "https://rickandmortyapi.com/api/episode/49",
+          "https://rickandmortyapi.com/api/episode/50",
+          "https://rickandmortyapi.com/api/episode/51"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/1",
+        "created": "2017-11-04T18:48:46.250Z"
+      },
+      {
+        "id": 2,
+        "name": "Morty Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Male",
+        "origin": {"name": "unknown", "url": ""},
+        "location": {
+          "name": "Citadel of Ricks",
+          "url": "https://rickandmortyapi.com/api/location/3"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        "episode": [
+          "https://rickandmortyapi.com/api/episode/42",
+          "https://rickandmortyapi.com/api/episode/43",
+          "https://rickandmortyapi.com/api/episode/44",
+          "https://rickandmortyapi.com/api/episode/45",
+          "https://rickandmortyapi.com/api/episode/46",
+          "https://rickandmortyapi.com/api/episode/47",
+          "https://rickandmortyapi.com/api/episode/48",
+          "https://rickandmortyapi.com/api/episode/49",
+          "https://rickandmortyapi.com/api/episode/50",
+          "https://rickandmortyapi.com/api/episode/51"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/2",
+        "created": "2017-11-04T18:50:21.651Z"
+      },
+      {
+        "id": 3,
+        "name": "Summer Smith",
+        "status": "Alive",
+        "species": "Human",
+        "type": "",
+        "gender": "Female",
+        "origin": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "location": {
+          "name": "Earth (Replacement Dimension)",
+          "url": "https://rickandmortyapi.com/api/location/20"
+        },
+        "image": "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        "episode": [
+          "https://rickandmortyapi.com/api/episode/47",
+          "https://rickandmortyapi.com/api/episode/48",
+          "https://rickandmortyapi.com/api/episode/49",
+          "https://rickandmortyapi.com/api/episode/51"
+        ],
+        "url": "https://rickandmortyapi.com/api/character/3",
+        "created": "2017-11-04T19:09:56.428Z"
+      }
+    ]
+  };
+}
